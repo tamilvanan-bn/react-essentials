526afbf39a56546f23c5fbb5077e986881a0d954
Tamilvanan <tamilvanan.b@outlook.com>
2026-08-11 10:33:09 +0530

    custom hook

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,18 @@
 import { MenuItem } from './MenuItem';
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from './UserContext';
+import { useDocTitle } from '../hooks/useDocTitle';
 
 export const Menu = () => {
   const handleOrder = (itemName, itemPrice) => {
     console.log(`You ordered ${itemName} for ${itemPrice}`);
   };
+  const [count, setCount] = useState(0);
   const user = useContext(UserContext);
+
+  useDocTitle(count);
+
   return (
     <div>
       <h2>{user.name}</h2>
@@ -15,6 +20,9 @@ export const Menu = () => {
       <MenuItem name='Pizza' price={12} onOrder={handleOrder} />
       <MenuItem name='Burger' price={8} onOrder={handleOrder} />
       <MenuItem name='Salad' price={6} onOrder={handleOrder} />
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Count: {count}
+      </button>
     </div>
   );
 };
